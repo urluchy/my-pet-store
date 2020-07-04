554bd59fc3354bc90c4f645d025f4ea57872e3ef
Ursula Okolie <45493240+urluchy@users.noreply.github.com>
2020-07-04 22:18:21 +0100

    Connected the project to an API

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -9,8 +9,11 @@
 </template>
 
 <script>
+import axios from "axios";
 import { Dogs } from "../data/dogs";
 import Dog from "../components/Dog.vue";
+
+axios.defaults.baseURL = "https://dog.ceo/api";
 
 export default {
   components: {
@@ -20,6 +23,33 @@ export default {
     return {
       dogs: Dogs
     };
+  },
+  created() {
+     this.dogs.forEach(dog => {
+     dog.img = "";
+  });
+    const linksArray = this.dogs.map(
+        dog => "/breed/" + dog.breed + "/images/random"
+      );
+       axios.all(linksArray.map(link => axios.get(link)))
+   .then(
+     axios.spread((...res) => {
+       this.dogs.forEach((dog, index) => {
+         dog.img = res[index].data.message;
+       });
+     })
+   );
+    axios
+      .get("/breed/husky/images/random")
+      .then(response => {
+        const husky = this.dogs.find(dog => dog.breed === "husky");
+        console.log(husky);
+        husky.img = response.data.message;
+      })
+
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
 </script>
