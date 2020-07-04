67dccc2e1a8dce2a842387e72b0647b90f3870f3
Ursula Okolie <45493240+urluchy@users.noreply.github.com>
2020-07-04 18:22:20 +0100

    added router, created a new page and added navigation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "@/plugins/vuetify";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
+import Pets from "./views/Pets";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/pets",
+    component: Pets
+  }
+];
+const router = new VueRouter({ routes });
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/views/Pets.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container grid-list-md fluid>
+    <v-layout wrap>
+      <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
+        <app-dog :dog="pet"></app-dog>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { Dogs } from "../data/dogs";
+import Dog from "../components/Dog.vue";
+
+export default {
+  components: {
+    appDog: Dog
+  },
+  data() {
+    return {
+      dogs: Dogs
+    };
+  }
+};
+</script>
+
+
+
+<style scoped>
+p {
+  margin: 0;
+}
+</style>
